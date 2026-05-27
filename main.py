d6c83582d241b78b7fb85ab46d88e15634e1f575
pikitosdoritos <fastovetsn@gmail.com>
2026-05-27 17:28:55 +0300

    Remove commented-out code and clean up line generation logic in main.py

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
-i = 1
+# i = 1
 
-while i < 9:
-    j = 1
-    line = ''
+# while i < 9:
+#     j = 1
+#     line = ''
     
-    while j < 9:
-        if j % 2 == 0:
-            line += '   '
+#     while j < 9:
+#         if j % 2 == 0:
+#             line += '   '
+#         else:
+#             line += '▮▮'
+
+#         j += 1
+    
+#     if i % 2 == 0:
+#         line = line[2:]
+
+#     print(line)
+    
+#     i += 1
+    
+y = 1
+
+svaston = []
+
+while y < 21:
+    x = 1
+    lines = ""
+    while x < 21:
+        if x == 10 and y == 10:
+            lines += "▮▮"
+        elif x == 10:
+            lines += "▮▮"
+        elif y == 10:
+            lines += "▮▮"
+        elif y == 1 and x >= 11:
+            lines += "▮▮"
+        elif y >= 10 and x == 20:
+            lines += "▮▮"
+        elif 1 <= y <= 10 and x == 1:
+            lines += "▮▮"
+        elif y == 20 and 0 < x < 11:
+            lines += "▮▮"
         else:
-            line += '▮▮'
-
-        j += 1
+            lines += "  "
+        
+        x += 1
     
-    if i % 2 == 0:
-        line = line[2:]
-
-    print(line)
+    svaston.append(lines)   
+    y += 1
     
-    i += 1
+    print(lines)
+
