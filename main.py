7915380f31697a779471f0b1833c4c36e9e08e21
pikitosdoritos <fastovetsn@gmail.com>
2026-05-27 16:54:00 +0300

    Refactor nested loops for line generation in main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 i = 1
-j = 0
+
 while i < 9:
+    j = 1
+    line = ''
+    
+    while j < 9:
+        if j % 2 == 0:
+            line += '   '
+        else:
+            line += '▮▮'
+
+        j += 1
+    
     if i % 2 == 0:
-        print('   ▮▮   ▮▮   ▮▮   ▮▮')
-    else:
-        print('▮▮   ▮▮   ▮▮   ▮▮   ')
-        
+        line = line[2:]
+
+    print(line)
+    
     i += 1
